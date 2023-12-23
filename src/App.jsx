977c31aafab406f308744110123d6e74a3a9b082
Angel Guzman <angelguzman10911@gmail.com>
2023-12-23 00:37:57 -0500

    se crea el proyecto Arca

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+const App = () => {
+  return (
+    <>
+    <div className=" bg-black h-[100vh]">  
+    <h1 className=" font-extrabold text-white flex justify-center items-center">
+      ARCA
+    </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
